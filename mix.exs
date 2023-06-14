fbad3e4c1ce7830ab9c185479a4938b618301e7d
Johan-Fast <johanr@fastcomm.com>
2023-06-14 14:31:44 +0200

    Distillery dependency update for otp 25

--- mix.exs
+++ mix.exs
@@ -6,11 +6,12 @@ defmodule Edeliver.Mixfile do
       app: :edeliver,
       version: "1.8.0",
       elixir: ">= 1.10.0",
-      description:  "Build and Deploy Elixir Applications and perform Hot-Code Upgrades and Schema Migrations",
+      description:
+        "Build and Deploy Elixir Applications and perform Hot-Code Upgrades and Schema Migrations",
       elixirc_paths: elixirc_paths(),
       package: [
         licenses: ["MIT"],
-        files:  [
+        files: [
           "bin",
           "CHANGELOG.md",
           "lib",
@@ -18,40 +19,45 @@ defmodule Edeliver.Mixfile do
           "mix.exs",
           "src",
           "strategies",
-          "README.md",
+          "README.md"
         ],
         maintainers: [],
-        links: %{"GitHub" => "https://github.com/boldpoker/edeliver"},
+        links: %{"GitHub" => "https://github.com/boldpoker/edeliver"}
       ],
       deps: deps(),
-      docs: docs(),
+      docs: docs()
     ]
   end
 
-  def application, do:
-    [applications: [],
-     mod: {Edeliver, []},
-     registered: [Edeliver.Supervisor, Edeliver],
-     env: []
-   ]
+  def application,
+    do: [
+      applications: [],
+      mod: {Edeliver, []},
+      registered: [Edeliver.Supervisor, Edeliver],
+      env: []
+    ]
 
   defp deps do
     [
-      {:distillery, "~> 2.1.0", optional: true, warn_missing: false},
+      {:distillery,
+       git: "https://github.com/archethic-foundation/distillery.git",
+       optional: true,
+       warn_missing: false},
       {:meck, "~> 0.8.13", only: :test},
       {:earmark, "~> 1.0", only: :dev},
-      {:ex_doc, "~> 0.19", only: :dev},
+      {:ex_doc, "~> 0.19", only: :dev}
     ]
   end
 
-  defp docs, do: [
-    logo: "docs/logo.png",
-    extras: [
-      "README.md": [title: "Usage"],
-      "docs/auto-versioning.md": [title: "Auto-Versioning"],
-      "docs/relup-patching.md": [title: "Relup-Patching"],
+  defp docs,
+    do: [
+      logo: "docs/logo.png",
+      extras: [
+        "README.md": [title: "Usage"],
+        "docs/auto-versioning.md": [title: "Auto-Versioning"],
+        "docs/relup-patching.md": [title: "Relup-Patching"]
+      ]
     ]
-  ]
 
   defp elixirc_paths() do
     [
